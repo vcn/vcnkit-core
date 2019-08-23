85bc2b21b96d6fd08587718be90e55cb116de54b
Freek Post <fpost@vcn.nl>
2019-08-23 14:59:44 +0200

    Fix InsertDriveFile

--- src/Icons/InsertDriveFile.js
+++ src/Icons/InsertDriveFile.js
@@ -8,10 +8,11 @@ export default props => (
         { ...props }
     >
         <path
-            fill="none" d="M0 0h24v24H0V0z"
+            d="M6 2c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6H6zm7 7V3.5L18.5 9H13z"
         />
         <path
-            d="M19 15l-6 6-1.42-1.42L15.17 16H4V4h2v10h9.17l-3.59-3.58L13 9l6 6z"
+            d="M0 0h24v24H0z"
+            fill="none"
         />
     </svg>
 );
